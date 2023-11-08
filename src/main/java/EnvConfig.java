441fbccfe93073186eef94be3e5334c3c0e18a9c
Daria Dubkova <rokston@mail.ru>
2023-11-09 01:55:20 +0300

    3rd fix commit

--- src/main/java/EnvConfig.java
+++ src/main/java/EnvConfig.java
@@ -1,6 +1,4 @@
 public class EnvConfig {
-    public static final String CHROME_DRIVER = System.getProperty("webdriver.chrome.driver", "src/test/resources/chrome/chromedriver.exe");
-    public static final String CHROME_BINARY = System.getProperty("webdriver.chrome.binary", "C://Praktikum/chrome-win64/chrome.exe");
     public static final String YANDEX_DRIVER = System.getProperty("webdriver.yandex.driver", "src/test/resources/yandex/chromedriver.exe");
     public static final String YANDEX_BINARY = System.getProperty("webdriver.yandex.binary", "src/test/resources/yandex/Application/browser.exe");
 }
